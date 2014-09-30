7f496fd60eba71d124aed74bda84f16431c9d1f2
OrenKishon <orenkishon1@gmail.com>
2014-09-30 14:14:09 +0300

    apron commented

--- worklist.cpp
+++ worklist.cpp
@@ -13,77 +13,74 @@
 #include "clang/Analysis/Analyses/DataflowWorklist.h"
 
 // Apron includes
-#include "ap_global0.h"
-#include "ap_global1.h"
-#include "box.h"
-#include "oct.h"
-#include "pk.h"
-#include "pkeq.h"
-
-using namespace clang;
+// #include "ap_global0.h"
+// #include "ap_global1.h"
+// #include "box.h"
+// #include "oct.h"
+// #include "pk.h"
+// #include "pkeq.h"
 
 static int printCFG;
 static const char *funcToAnalyze;
-static ap_manager_t* man;
-static ap_environment_t *env;
+// static ap_manager_t* man;
+// static ap_environment_t *env;
 
 class Variables {
     // Maps a variable to an apron 'dimention' (index)
-    std::unordered_map<const VarDecl *, unsigned> var2Dim;
-    DeclContext *dc;
-    ap_var_t *name_of_dim;
+    std::unordered_map<const clang::VarDecl *, unsigned> var2Dim;
+    clang::DeclContext *dc;
+//    ap_var_t *name_of_dim;
     int size;
     
 public:
     Variables() {
-        name_of_dim = NULL;
+//        name_of_dim = NULL;
         size = 0;
     }
 
-    void init(Decl *D) {
-        dc = cast<DeclContext>(D);
+    void init(clang::Decl *D) {
+        dc = clang::cast<clang::DeclContext>(D);
         
-        for (DeclContext::specific_decl_iterator<VarDecl> I(dc->decls_begin()),
+        for (clang::DeclContext::specific_decl_iterator<clang::VarDecl> I(dc->decls_begin()),
                 E(dc->decls_end()); I != E; ++I) {
-            const VarDecl *vd = *I;
+            const clang::VarDecl *vd = *I;
 
             if (isTrackedVar(vd))
                 ++size;
         }
 
-        name_of_dim = new ap_var_t[size];
+//        name_of_dim = new ap_var_t[size];
 
         int ind = 0;
-        for (DeclContext::specific_decl_iterator<VarDecl> I(dc->decls_begin()),
+        for (clang::DeclContext::specific_decl_iterator<clang::VarDecl> I(dc->decls_begin()),
                 E(dc->decls_end()); I != E; ++I) {
-            const VarDecl *vd = *I;
+            const clang::VarDecl *vd = *I;
 
             if (!isTrackedVar(vd)) {
                 continue;
             }
 
-            name_of_dim[ind] = (ap_var_t)vd->getNameAsString().c_str();
+//            name_of_dim[ind] = (ap_var_t)vd->getNameAsString().c_str();
             var2Dim[vd] = ind;
             ++ind;
         }
 
         // Assuming integers only;
-        env = ap_environment_alloc(name_of_dim, size, NULL, 0);
+//        env = ap_environment_alloc(name_of_dim, size, NULL, 0);
     }
 
     ~Variables() {
-        if (env)
-            ap_environment_free(env);
-        if (name_of_dim) {
-            delete [] name_of_dim;
-        }
+//        if (env)
+//            ap_environment_free(env);
+//        if (name_of_dim)
+//            delete [] name_of_dim;
     }
 
-    bool isTrackedVar(const VarDecl *vd) {
+    bool isTrackedVar(const clang::VarDecl *vd) {
         if (vd->isLocalVarDecl() && !vd->hasGlobalStorage() &&
                 !vd->isExceptionVariable() && !vd->isInitCapture() &&
                 vd->getDeclContext() == dc) {
-            QualType ty = vd->getType();
+            clang::QualType ty = vd->getType();
             
             return ty->isScalarType() || ty->isVectorType();
         }
@@ -96,29 +93,29 @@ static Variables variables;
 
 // This context exists for every block
 class BlockAnalysisContext {
-    const CFGBlock *block;
+    const clang::CFGBlock *block;
     // A reference to the global map
-    std::unordered_map<const CFGBlock *, BlockAnalysisContext *> *block2Ctx;
+    std::unordered_map<const clang::CFGBlock *, BlockAnalysisContext *> *block2Ctx;
 
     // Abstract values
-    std::unordered_map<const CFGBlock *, ap_abstract1_t> pred2Abs;
-    ap_abstract1_t abs;
+//    std::unordered_map<const CFGBlock *, ap_abstract1_t> pred2Abs;
+//    ap_abstract1_t abs;
 
     // Successors
-    const CFGBlock *succ[2];
+    const clang::CFGBlock *succ[2];
 public:
-    BlockAnalysisContext(const CFGBlock *block,
-            std::unordered_map<const CFGBlock *, BlockAnalysisContext *> *block2Ctx) {
+    BlockAnalysisContext(const clang::CFGBlock *block,
+            std::unordered_map<const clang::CFGBlock *, BlockAnalysisContext *> *block2Ctx) {
         this->block = block;
         this->block2Ctx = block2Ctx; 
 
         // Chaotic iteration: All abstract values initialized as bottom
-        for (CFGBlock::const_pred_iterator I = block->pred_begin(), E = block->pred_end(); I != E; ++I) {
-            const CFGBlock *Pred = *I;
+        for (clang::CFGBlock::const_pred_iterator I = block->pred_begin(), E = block->pred_end(); I != E; ++I) {
+            const clang::CFGBlock *Pred = *I;
             if (!Pred)
                 continue;
 
-            pred2Abs[Pred] = ap_abstract1_bottom(man, env);
+//            pred2Abs[Pred] = ap_abstract1_bottom(man, env);
         }
 
         assert(block->succ_size() <= 2);
@@ -130,8 +127,8 @@ public:
         if (block->succ_empty())
             return;
 
-        for (CFGBlock::const_succ_iterator I = block->succ_begin(), E = block->succ_end(); I != E; ++I) {
-            const CFGBlock *s = *I;
+        for (clang::CFGBlock::const_succ_iterator I = block->succ_begin(), E = block->succ_end(); I != E; ++I) {
+            const clang::CFGBlock *s = *I;
             if (!s)
                 continue;
 
@@ -145,21 +142,21 @@ public:
     void updateEntryValue() {
         // Entry block
         if (block->pred_empty()) {
-            abs = ap_abstract1_top(man, env);
+//            abs = ap_abstract1_top(man, env);
             return;
         }
 
         // Merge (join) all predeseccors exit values
         
         printf("%d) Merging preds' values. old value:", block->getBlockID());
-        ap_abstract1_fprint(stdout, man, &abs);
-        abs = ap_abstract1_bottom(man, env);
-        for (auto it = pred2Abs.begin(); it != pred2Abs.end(); ++it) {
-            ap_abstract1_t absPred = it->second;
-            abs = ap_abstract1_join(man, false, &abs, &absPred);
-        }
-        printf("After merge:");
-        ap_abstract1_fprint(stdout, man, &abs);
+//        ap_abstract1_fprint(stdout, man, &abs);
+//        abs = ap_abstract1_bottom(man, env);
+//        for (auto it = pred2Abs.begin(); it != pred2Abs.end(); ++it) {
+//            ap_abstract1_t absPred = it->second;
+//            abs = ap_abstract1_join(man, false, &abs, &absPred);
+//        }
+//        printf("After merge:");
+//        ap_abstract1_fprint(stdout, man, &abs);
     }
 
     // Each successor holds a list of its predecessors exit values. We update the entry for this block in each
@@ -170,7 +167,7 @@ public:
 
         // Single successor: just pass this block's exit value as the successor's entry value
         if (!block->getTerminator()) {
-            (*block2Ctx)[succ[0]]->pred2Abs[block] = abs;
+//            (*block2Ctx)[succ[0]]->pred2Abs[block] = abs;
             return;
         }
 
@@ -180,17 +177,17 @@ public:
         cond->dump();
 
         // XXX: Do something with cond
-        (*block2Ctx)[succ[0]]->pred2Abs[block] = abs;
-        (*block2Ctx)[succ[1]]->pred2Abs[block] = abs;
+//        (*block2Ctx)[succ[0]]->pred2Abs[block] = abs;
+//        (*block2Ctx)[succ[1]]->pred2Abs[block] = abs;
     }
 
     void printAbs() {
         printf("Abs value: ");
-        ap_abstract1_fprint(stdout, man, &abs);
+//        ap_abstract1_fprint(stdout, man, &abs);
     }
 };
 
-class TransferFunctions : public StmtVisitor<TransferFunctions> {
+class TransferFunctions : public clang::StmtVisitor<TransferFunctions> {
     BlockAnalysisContext *analysisCtx;
 
 public:
@@ -207,20 +204,20 @@ public:
 //  void VisitDeclRefExpr(DeclRefExpr *dr);
 //  void VisitObjCForCollectionStmt(ObjCForCollectionStmt *FS);
 //  void VisitObjCMessageExpr(ObjCMessageExpr *ME);
-    void VisitBinaryOperator(BinaryOperator *BO) {
-        if (BO->getOpcode() == BO_Assign) {
+    void VisitBinaryOperator(clang::BinaryOperator *BO) {
+        if (BO->getOpcode() == clang::BO_Assign) {
             printf("Assignment\n");
             BO->getLHS()->dump();
             BO->getRHS()->dump();
         }
     }
     
-    void VisitDeclStmt(DeclStmt *DS) {
+    void VisitDeclStmt(clang::DeclStmt *DS) {
         printf("DeclStmt:\n");
         DS->getSingleDecl()->dump();
     
         for (auto *DI : DS->decls()) {
-            VarDecl *VD = dyn_cast<VarDecl>(DI);
+            clang::VarDecl *VD = clang::dyn_cast<clang::VarDecl>(DI);
             VD->dump();
         }
     }
@@ -228,15 +225,15 @@ public:
 
 class BlockAnalysis {
     // A container for BlockAnalysisContext objects
-    std::unordered_map<const CFGBlock *, BlockAnalysisContext *> block2Ctx;
+    std::unordered_map<const clang::CFGBlock *, BlockAnalysisContext *> block2Ctx;
 
 public:
-    void add(const CFGBlock *block) {
+    void add(const clang::CFGBlock *block) {
         block2Ctx[block] = new BlockAnalysisContext(block, &block2Ctx);
     }
 
     ~BlockAnalysis() {
-        for (std::unordered_map<const CFGBlock *, BlockAnalysisContext *>::
+        for (std::unordered_map<const clang::CFGBlock *, BlockAnalysisContext *>::
                 iterator I = block2Ctx.begin(), E = block2Ctx.end();
                 I != E; ++I) {
             BlockAnalysisContext *ctx = (*I).second;
@@ -247,7 +244,7 @@ public:
         }
     }
 
-    bool runOnBlock(const CFGBlock *block) {
+    bool runOnBlock(const clang::CFGBlock *block) {
         printf("Run on block B[%d]\n", block->getBlockID());
 
         block2Ctx[block]->updateEntryValue();
@@ -256,9 +253,9 @@ public:
     //    TransferFunctions tf(vals, cfg, block, ac, classification, handler);
         TransferFunctions tf(block2Ctx[block]);
     
-        for (CFGBlock::const_iterator I = block->begin(), E = block->end();
+        for (clang::CFGBlock::const_iterator I = block->begin(), E = block->end();
                 I != E; ++I) {
-            if (Optional<CFGStmt> cs = I->getAs<CFGStmt>())
+            if (clang::Optional<clang::CFGStmt> cs = I->getAs<clang::CFGStmt>())
                 tf.Visit(const_cast<clang::Stmt*>(cs->getStmt()));
         }
     
@@ -268,7 +265,7 @@ public:
     }
 };
 
-static void VisualizeCfg(AnalysisDeclContext *ac, CFG *cfg) {
+static void VisualizeCfg(clang::AnalysisDeclContext *ac, clang::CFG *cfg) {
     switch (printCFG) {
     case 1:
         ac->dumpCFG(false);
@@ -276,33 +273,33 @@ static void VisualizeCfg(AnalysisDeclContext *ac, CFG *cfg) {
     case 2:
         // Child process, pops up a graphic tool to show the graph
         if (fork() == 0) {
-            cfg->viewCFG(LangOptions());
+            cfg->viewCFG(clang::LangOptions());
             exit(0);
         }
         break;
     }
 }
 
-static void analyze(Decl *D) {
+static void analyze(clang::Decl *D) {
     variables.init(D);
 
-    AnalysisDeclContext ac(/* AnalysisDeclContextManager */ nullptr, D);
-    CFG *cfg;
+    clang::AnalysisDeclContext ac(/* AnalysisDeclContextManager */ nullptr, D);
+    clang::CFG *cfg;
     if (!(cfg = ac.getCFG()))
         exit(1);
 
     VisualizeCfg(&ac, cfg);
 
-    ForwardDataflowWorklist worklist(*cfg, ac);
+    clang::ForwardDataflowWorklist worklist(*cfg, ac);
     worklist.enqueueSuccessors(&cfg->getEntry());
 
     llvm::BitVector previouslyVisited(cfg->getNumBlockIDs());
 
     BlockAnalysis blockAnalysis;
 
-    for (CFG::const_iterator BI = cfg->begin(), BE = cfg->end(); BI != BE;
+    for (clang::CFG::const_iterator BI = cfg->begin(), BE = cfg->end(); BI != BE;
             ++BI) {
-        const CFGBlock *block = *BI;
+        const clang::CFGBlock *block = *BI;
 
         blockAnalysis.add(block);
     }
@@ -310,7 +307,7 @@ static void analyze(Decl *D) {
 
     blockAnalysis.runOnBlock(&cfg->getEntry());
 
-    while (const CFGBlock *block = worklist.dequeue()) {
+    while (const clang::CFGBlock *block = worklist.dequeue()) {
         printf("Block ID: %u\n", block->getBlockID());
 
         // Did the block change?
@@ -322,41 +319,41 @@ static void analyze(Decl *D) {
     }     
 }
 
-class ExampleVisitor : public RecursiveASTVisitor<ExampleVisitor> {
+class ExampleVisitor : public clang::RecursiveASTVisitor<ExampleVisitor> {
 private:
-    ASTContext *astContext; // used for getting additional AST info
+    clang::ASTContext *astContext; // used for getting additional AST info
 
 public:
-    explicit ExampleVisitor(CompilerInstance *CI) 
+    explicit ExampleVisitor(clang::CompilerInstance *CI) 
       : astContext(&(CI->getASTContext())) // initialize private members
     { }
 
-    virtual bool VisitFunctionDecl(FunctionDecl *func) {
+    virtual bool VisitFunctionDecl(clang::FunctionDecl *func) {
         std::string funcName = func->getNameInfo().getName().getAsString();
         llvm::outs() << funcName << "\n";        
         return true;
     }
 };
 
-class ExampleASTConsumer : public ASTConsumer {
+class ExampleASTConsumer : public clang::ASTConsumer {
 private:
     ExampleVisitor *visitor; // doesn't have to be private
 
 public:
     // override the constructor in order to pass CI
     // initialize the visitor
-    explicit ExampleASTConsumer(CompilerInstance *CI) : visitor(
+    explicit ExampleASTConsumer(clang::CompilerInstance *CI) : visitor(
             new ExampleVisitor(CI)) {
     }
 
     // override this to call our ExampleVisitor on each top-level Decl
-    virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
+    virtual bool HandleTopLevelDecl(clang::DeclGroupRef DG) {
         if (!DG.isSingleDecl()) {
            return true;
         }
 
-        Decl *D = DG.getSingleDecl();
-        const FunctionDecl *FD = dyn_cast<FunctionDecl>(D);
+        clang::Decl *D = DG.getSingleDecl();
+        const clang::FunctionDecl *FD = clang::dyn_cast<clang::FunctionDecl>(D);
         // Skip other functions
         if (!FD || FD->getName().str().compare(funcToAnalyze)) {
             return true;
@@ -371,10 +368,10 @@ public:
     }
 };
 
-class ExampleFrontendAction : public ASTFrontendAction {
+class ExampleFrontendAction : public clang::ASTFrontendAction {
 public:
-    virtual std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
-            StringRef file) {
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &CI,
+            clang::StringRef file) {
         // pass CI pointer to ASTConsumer
         return llvm::make_unique<ExampleASTConsumer>(&CI);
     }
@@ -400,15 +397,15 @@ int main(int argc, const char **argv) {
     std::string contents((std::istreambuf_iterator<char>(in)), 
             std::istreambuf_iterator<char>());
 
-    man = box_manager_alloc();
+//    man = box_manager_alloc();
     printf("******************************\n");
-    printf("Apron: Library %s, version %s\n", man->library, man->version);
+//    printf("Apron: Library %s, version %s\n", man->library, man->version);
     printf("******************************\n");
 
     int ret = !clang::tooling::runToolOnCode(new ExampleFrontendAction,
             contents.c_str());
 
-    ap_manager_free(man);
+//    ap_manager_free(man);
 
     return ret;
 }
